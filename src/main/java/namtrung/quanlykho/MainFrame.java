958b7fa36e97125bdf13bcac8967ab61ddc97fb2
llalz54 <vanducthanh0209@gmail.com>
2025-06-13 08:07:32 +0700

    h

--- src/main/java/namtrung/quanlykho/MainFrame.java
+++ src/main/java/namtrung/quanlykho/MainFrame.java
@@ -1,9 +1,13 @@
 package namtrung.quanlykho;
 
+import com.itextpdf.awt.geom.AffineTransform;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
@@ -13,6 +17,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 /**
@@ -29,7 +34,6 @@ public class MainFrame extends javax.swing.JFrame {
         initComponents();
         applyDefaultUIStyle();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         // Lấy kích thước màn hình
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Dimension baseSize = new Dimension(1500, 800); // kích thước thiết kế chuẩn
@@ -44,11 +48,11 @@ public class MainFrame extends javax.swing.JFrame {
         applyScaleFactor(scaleFactor);
 
         // Tính toán kích thước tối đa (90% màn hình hoặc 1500x800, tùy cái nào nhỏ hơn)
-      //  int maxWidth = Math.min(1500, (int) (screenSize.width * 0.9));
-      //  int maxHeight = Math.min(800, (int) (screenSize.height * 0.9));
-
+        //  int maxWidth = Math.min(1500, (int) (screenSize.width * 0.9));
+        //  int maxHeight = Math.min(800, (int) (screenSize.height * 0.9));
         // Thiết lập kích thước
-    //    setSize(maxWidth, maxHeight);
+        //    setSize(maxWidth, maxHeight);
+        pack();
         setLocationRelativeTo(null); // căn giữa màn hình
 
         // Thiết lập icon
@@ -724,6 +728,7 @@ public class MainFrame extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -749,6 +754,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+
             public void run() {
                 new MainFrame().setVisible(true);
             }
